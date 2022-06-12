df89648961423f5249de0fb8b77fa46759c02e2f
dev_Flo <f.teutsch@marketvision.ai>
2022-06-12 14:32:41 +0200

    Make roberta deployable but only with real time instance

--- torchmaker/examples/roberta/deployment/deploy.py
+++ torchmaker/examples/roberta/deployment/deploy.py
@@ -4,36 +4,18 @@ from sagemaker.huggingface import HuggingFaceModel
 from sagemaker.serializers import JSONSerializer
 from sagemaker.deserializers import JSONDeserializer
 
-# model = PyTorchModel(
-#     entry_point="inference.py",
-#     source_dir="code",
-#     role=config.SAGEMAKER_ROLE,
-#     model_data=config.S3_ROBERTA_MODEL_DATA_PATH,
-#     framework_version="1.5.0",
-#    #transformers_version="4.17", # transformers version used
-#    #pytorch_version="1.10", # pytorch version used
-#     py_version="py3",
-# )
-
-#question-answering
-model = HuggingFaceModel(
-   env={"HF_TASK": "feature-extraction"}, #"HF_MODEL_ID":'T-Systems-onsite/cross-en-de-roberta-sentence-transformer',
-   #source_dir="code",
-   model_data=config.S3_ROBERTA_MODEL_DATA_PATH,  # path to your trained SageMaker model
-   role=config.SAGEMAKER_ROLE,                                            # IAM role with permissions to create an endpoint
-   #transformers_version="4.6",                           # Transformers version used
-   #pytorch_version="1.7",                                # PyTorch version used
-   #py_version='py36',                                    # Python version used
-    transformers_version="4.17",  # transformers version used
-    pytorch_version="1.10",  # pytorch version used
-    py_version='py38',  # Python version used
-
+model = PyTorchModel(
+    entry_point="inference.py",
+    role=config.SAGEMAKER_ROLE,
+    model_data=config.S3_ROBERTA_MODEL_DATA_PATH,
+    framework_version="1.5.0",
+    py_version="py3",
 )
 
 if False: #config.AWS_SAGEMAKER_SERVERLESS:
     from sagemaker.serverless import ServerlessInferenceConfig
 
-    serverless_config = ServerlessInferenceConfig(memory_size_in_mb=4096,
+    serverless_config = ServerlessInferenceConfig(memory_size_in_mb=2048,
                                                   max_concurrency=1)  # max_concurrency=3 3 was not possible with high torch version
 
     predictor = model.deploy(
@@ -45,7 +27,7 @@ else:
     predictor = model.deploy(
         endpoint_name=config.AWS_SAGEMAKER_ROBERTA_ENDPOINT_NAME,
         initial_instance_count=1,
-        instance_type="ml.m5.xlarge", # config.AWS_SAGEMAKER_INSTANCE_TYPE,
+        instance_type="ml.c5.large", # "ml.m5.xlarge", # config.AWS_SAGEMAKER_INSTANCE_TYPE,
         serializer=JSONSerializer(),
         deserializer=JSONDeserializer(),
     )
